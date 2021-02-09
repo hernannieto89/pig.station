21c3868440144e4697d71239110e7e6673667cc1
hnieto <hernan.nieto89@gmail.com>
2021-02-09 20:54:40 -0300

    rpyc rule

--- app/storage/rules/base_rule.py
+++ app/storage/rules/base_rule.py
@@ -1,81 +1,60 @@
+import rpyc
 import json
 import requests
 
 from app.storage import db
-from app.storage.sensors import Sensor
-from app.storage.actions import Action
 
-VALID_BOOLEANS = ["True", "False"]
-VALID_CONNECTORS = ["GT", "LT", "EQ", "GE", "LE"]
-
-# CLOCK-1-H_GT_6, CLOCK-1-H_LT_2
-
-# DHT22-1-H_GT_30, 
- 
-SENSOR_TEMPLATE = "{sensor_type}-{sensor_id}-{sensor_metric}" # DHT22-1-H: Get humidity from DHT22 SENSOR WITH ID=1
-
-ACTION_TEMPLATE = "{action_type}-{action_id}-{action_mode}" # RELAY-1-ON
-
-SENSOR_URL_TEMPLATE = "http://localhost:5000/sensors/{sensor_type}/{sensor_id}"
-
-ACTION_URL_TEMPLATE = "http://localhost:5000/actions/{action_type}/{action_id}/{action_mode}"
 
 class Rule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     job_id = db.Column(db.String(128))
-    frecuency = db.Column(db.String(128), nullable=False)
+    frecuency = db.Column(db.String(128), default=None, nullable=True)
+    cron_frecuency = db.Column(db.String(128), default=None, nullable=True)
     conditions = db.Column(db.String(128), default="[]")
     actions_dict = db.Column(db.String(128), default="{}")
     relays_used = db.Column(db.String(128), default="[]")
     active = db.Column(db.Boolean, default=True)
+    job_id = db.Column(db.String(128), default=None, nullable=True)
+    rule_type = db.Column(db.String(128), default="interval")
 
-    def run(self):
-        rule_value = self._parse_conditions()
-        action_template = json.loads(self.actions_dict)[rule_value]
-        self._perform_action(action_template)
+    def start_job(self):
+        job_args = [self.conditions, self.actions_dict]
+        job_kwargs = self._parse_kwargs(self.rule_type, self.frecuency)
+        if self.job_id is None:
+            conn = rpyc.connect('localhost', 12345)
+            job = conn.root.add_job(self.rule_type, job_args, **job_kwargs)
+            self.job_id = job.id
+            conn.close()
+            return "Job {} started".format(self.job_id)
+        return "Job {} already running".format(self.job_id)
 
-    def _perform_action(self, action_template):
-        action_type, action_id, action_mode = action_template.split("-")
-        # REPLACE WITH REQUEST!
-        url = ACTION_URL_TEMPLATE.format(action_type=action_type, action_id=action_id, action_mode=action_mode)
-        requests.post(url)
-
-    def _parse_conditions(self):
-        """
-        Returns True if all conditions al met. False otherwise.
-        """
-
-        result = []
-
-        for condition in json.loads(self.conditions):
-            splitted = condition.split("-")
-            if len(splitted) == 1:
-                result.append(bool(condition))
-            if len(splitted) == 3 and splitted[1] in VALID_CONNECTORS:
-                connector = splitted[1]
-                sensor_type, sensor_id, sensor_metric = splitted[0].split("-")
-                sensor_value = self._get_sensor_value(sensor_type, sensor_id, sensor_metric)
-                target_value = eval(splitted[2])
-                evaluated_condition = self._evaluate_condition(sensor_value, connector, target_value)
-                result.append(evaluated_condition)
-        if all(result):
-            return "True"
+    def stop_job(self):
+        job_id = self.job_id
+        if job_id is not None:
+            conn = rpyc.connect('localhost', 12345)
+            conn.root.remove_job(job_id)
+            conn.close()
+            self.job_id = None
+            return "Stoped Job {}".format(job_id)
         else:
-            return "False"
+            return "Job already stopped"
+        job_kwargs = {}
 
-    def _evaluate_condition(self, sensor_value, connector, target_value):
-        if connector == "GT": return sensor_value > target_value
-        elif connector == "GE": return sensor_value >= target_value
-        elif connector == "LT": return sensor_value < target_value
-        elif connector == "LE": return sensor_value <= target_value
-        elif connector == "EQ": return sensor_value == target_value
-
-    def _get_sensor_value(self, sensor_type, sensor_id, sensor_metric):
-        # REPLACE WITH REQUEST!
-        url = SENSOR_URL_TEMPLATE.format(sensor_type=sensor_type, sensor_id=sensor_id)
-        response = requests.get(url)
-        return response[sensor_metric]
-
-    def __repr__(self):
-        return f"<Rule: Conditions=({self.conditions}) - Actions=({self.actions_dict}) - RelaysUsed=({self.relays_used}) >"
+    def _parse_kwargs(rule_type, frecuency):
+        kwargs = {}
+        if rule_type == "interval" and frecuency is not None:
+            if "s" in rule.frecuency:
+                kwargs["seconds"] = eval(rule.frecuency.split("s")[0])
+            if "m" in rule.frecuency:
+                kwargs["minutes"] = eval(rule.frecuency.split("m")[0])
+            if "h" in rule.frecuency:
+                kwargs["hours"] = eval(rule.frecuency.split("h")[0])
+        else if rule_type == "cron" and cron_frecuency is not None:
+            if "s" in cron_frecuency:
+                kwargs["second"] = eval(rule.frecuency.split("s")[0])
+            if "m" in cron_frecuency:
+                kwargs["minute"] = eval(rule.frecuency.split("m")[0])
+            if "h" in cron_frecuency:
+                kwargs["hour"] = eval(rule.frecuency.split("h")[0])
+        return kwargs
