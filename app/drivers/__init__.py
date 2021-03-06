4ff60ab818ce8ae4719fadab7041f723dbab8698
hnieto <hernan.nieto89@gmail.com>
2021-03-05 22:32:14 -0300

    wip

--- app/drivers/__init__.py
+++ app/drivers/__init__.py
@@ -5,7 +5,12 @@ TYPE_TO_DRIVER = {
     "DHT11": "DHT11Driver"
 
 }
+
 SUPPORTED_SENSORS = list(TYPE_TO_DRIVER.keys())
 
+SENSORS_READ_RATE = {
+   "DHT22": 5,
+   "DHT11": 5
+}
 
 __all__ = ["TYPE_TO_DRIVER", "SUPPORTED_SENSORS"]
