261e1665eecd873c5df4d35c24a03ab0006a4647
hnieto <hernan.nieto89@gmail.com>
2022-06-10 20:57:05 -0300

    wip

--- app/config/development.py
+++ app/config/development.py
@@ -5,7 +5,7 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 class DevelopmentConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:////home/pi/test.db"
-    JOBS = [{"id": "periodic_read", "func": periodic_read, "trigger": "interval", "seconds": 20, "replace_existing": True}]
+    JOBS = [{"id": "periodic_read", "func": periodic_read, "trigger": "interval", "seconds": 300, "replace_existing": True}]
 
     SCHEDULER_JOBSTORES = {
         "default": SQLAlchemyJobStore(url="sqlite:////home/pi/jobstore.db")
