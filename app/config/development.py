bafd3e9991c1974f54c09f595895fa50a7416b9d
hnieto <hernan.nieto89@gmail.com>
2022-07-01 19:14:20 -0300

    Fixed time for scheduler

--- app/config/development.py
+++ app/config/development.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.historic import periodic_read
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
@@ -5,7 +6,7 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 class DevelopmentConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:////home/pi/test.db"
-    JOBS = [{"id": "periodic_read", "func": periodic_read, "trigger": "interval", "seconds": 300, "replace_existing": True}]
+    JOBS = [{"id": "periodic_read", "func": periodic_read, "trigger": "interval", "seconds": 3600, "replace_existing": True, "next_run_time" : datetime.now()}]
 
     SCHEDULER_JOBSTORES = {
         "default": SQLAlchemyJobStore(url="sqlite:////home/pi/jobstore.db")
